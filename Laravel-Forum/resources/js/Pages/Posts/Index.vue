be5e1781f4c85a96688eb94addd4fbc5ee07408a
tqt97 <kutuanonline199@gmail.com>
2024-03-18 15:12:06 +0700

    Testing dispay posts and edit AppLayout

---
+++ Laravel-Forum/resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,34 @@
+<template>
+    <AppLayout title="Posts">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Posts
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <Container>
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <h1>All posts</h1>
+                    <div class="mt-5">
+                        <ul>
+                            <li v-for="post in posts" :key="post.id">
+                                {{ post.title }}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    </AppLayout>
+</template>
+
+<script setup>
+import Container from '@/Components/Container.vue';
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+// defineProps({
+//     posts: Array
+// })
+defineProps(['posts'])
+</script>
